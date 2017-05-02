05227df49a8969630f2e2e8b4690392f8c00afdf
Mathias Bergqvist <mathias.bergqvist@gmail.com>
2017-05-02 23:09:01 +0200

    added transpiling, bundling and linting

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './index.css';
+import numeral from 'numeral';
+
+const val = numeral(1000).format('$0,0.00');
+console.log(`I would pay ${val} for this awesome course!`); // eslint-disable-line no-console
